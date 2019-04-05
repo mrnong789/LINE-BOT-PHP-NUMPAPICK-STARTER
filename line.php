64ca40e0851a744c1b07ae801e6fe4a7c3de6489
mrnong789 <mrnong789@gmail.com>
2019-04-05 23:48:53 +0700

    update user id

--- line.php
+++ line.php
@@ -14,7 +14,7 @@ function send_LINE($msg){
       $url = 'https://api.line.me/v2/bot/message/push';
       $data = [
 
-        //'to' => 'Ue77a191627f6ac91899e75d92264310c',
+        'to' => 'U55c766af592a614bdd88140c48191455',
         'messages' => [$messages],
       ];
       $post = json_encode($data);
