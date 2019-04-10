ea2dda80e9f3d45b6907e9f262bc99cb41099f09
mrnong789 <mrnong789@gmail.com>
2019-04-11 00:11:44 +0700

    send to group

--- line.php
+++ line.php
@@ -8,7 +8,7 @@ function send_LINE($msg){
       // Make a POST Request to Messaging API to reply to sender
       $url = 'https://api.line.me/v2/bot/message/push';
       $data = [
-        'to' => ['U3f20bb4d833cb40b8ac4d5e1c1f9c9ce','U55c766af592a614bdd88140c48191455'],         //PUT LINE ID at "Your user ID"
+        'to' => 'C4025d4f09c33c08f3f506f23eb08ddbc',         //PUT LINE ID at "Your user ID"
         'messages' => [$messages],
       ];
       $post = json_encode($data);
