3693bcbab99c1860d9c863d56921b9ffc81f3d59
mrnong789 <mrnong789@gmail.com>
2019-04-11 00:10:25 +0700

    get group id

--- user.php
+++ user.php
@@ -1,39 +1,4 @@
 <?php
-//  require("pub.php");
-//  require("line.php");
-// // Get POST body content
-// $content = file_get_contents('php://input');
-// // Parse JSON
-// $events = json_decode($content, true);
-// // Validate parsed JSON data
-// if (!is_null($events['ESP'])) {
-//    send_LINE($events['ESP']);
-//    echo "OK";
-// }
-// if (!is_null($events['events'])) {
-//    echo "line bot";
-//    // Loop through each event
-//    foreach ($events['events'] as $event) {
-// 	// Reply only when message sent is in 'text' format
-// 	if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-// 	   // Get text sent
-// 	   $text = $event['message']['text'];
-// 	   // Get replyToken
-// 	   $replyToken = $event['replyToken'];
-//   	   // Build message to reply back
-//     	$Topic = "ESP32" ;
-// 	   getMqttfromlineMsg($Topic,$text);
-// 	}
-//     }
-//  }
-//  echo "OK3";
-
-
-
-
-
-
-
 
 $strAccessToken = "MDogdQfR5pelyDFp24lerYcf3ehef8x/GfOt7PpFBH2HyHe/lCHMfgSaRKe3Bbc9pbc2hFKptcocK7RwR60ySMMri9pZxsskkuox7jeM5Y04YSHwA8T1wv33njtaz2ntxIOROcvUVXzmpqud8j3C0AdB04t89/1O/w1cDnyilFU=";
  
@@ -50,7 +15,7 @@ if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
-  $arrPostData['messages'][0]['text'] = "สวัสดี ID คุณคือ ".$arrJson['events'][0]['source']['userId'];
+  $arrPostData['messages'][0]['text'] = "สวัสดี ID คุณคือ ".$arrJson['events'][0]['source']['groupId'];
 }else if($arrJson['events'][0]['message']['text'] == "ชื่ออะไร"){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
