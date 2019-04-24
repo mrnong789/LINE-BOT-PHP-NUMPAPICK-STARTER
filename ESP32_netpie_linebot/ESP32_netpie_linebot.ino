bdc562b9e24d162730328cdc5aa6295aae7fce13
mrnong789 <mrnong789@gmail.com>
2019-04-25 00:09:12 +0700

    add ultrasonic and servo

--- ESP32_netpie_linebot/ESP32_netpie_linebot.ino
+++ ESP32_netpie_linebot/ESP32_netpie_linebot.ino
@@ -10,12 +10,20 @@
 const char* ssid     = "Nong-WiFi"; //change this to your SSID
 const char* password = "abcde12345"; //change this to your PASSWORD
 
-const char* host = "http://fitfarm.herokuapp.com/bot.php";//change this to your linebot server ex.http://numpapick-linebot.herokuapp.com/bot.php
+const char* host = "http://fitfarm.herokuapp.com/bot.php";
 #define APPID   "FitfarmLinebot"     //change this to your APPID
 #define KEY     "ToLd5f0617sX7LB"     //change this to your KEY
 #define SECRET  "uskIJuEFrKivJwTzjLrljz6ZY"     //change this to your SECRET
 
 #define ALIAS   "ESP32"                    //set name of device
+
+#define TRIGGER 5
+#define ECHO    4
+// NodeMCU Pin D1 > TRIGGER | Pin D2 > ECHO
+
+#include <Servo.h> // including servo library.
+
+Servo servo_1; // Giving name to servo.
 
 WiFiClient client;
 String uid = "";
@@ -27,13 +35,20 @@ void onMsghandler(char *topic, uint8_t* msg, unsigned int msglen) {
   msg[msglen] = '\0';
   Serial.println((char *)msg);
   String msgLINE = (char *)msg;
-  if ( msgLINE == "ON" || msgLINE == "On" || msgLINE == "on" ) {
-    digitalWrite(2, HIGH);         // LED on
-    send_json("LED Turn on");
+  if ( msgLINE == "ON" || msgLINE == "On" || msgLINE == "on" || msgLINE == "รดน้ำ" || msgLINE == "1") {
+    digitalWrite(2, LOW);         // LED on
+    send_json("เป่าเปารดน้ำให้แล้วฮ่ะ");
+  }else if ( msgLINE == "OFF" || msgLINE == "Off"  || msgLINE == "off" || msgLINE == "ปิดน้ำ" || msgLINE == "2") {
+    digitalWrite(2, HIGH);          // LED off
+    send_json("เป่าเปาปิดน้ำให้แล้วฮ่ะ");
+  }else if ( msgLINE == "กิน" || msgLINE == "อาหาร"  || msgLINE == "เอาไป" || msgLINE == "จ้า" || msgLINE == "3") {
+    servo_1.write (180);  // servo will move to 90 degree angle.
+    delay (3000);
+    servo_1.write (0); 
+    send_json("มะหมาได้กินแล้วฮ่ะ");
   }
-  if ( msgLINE == "OFF" || msgLINE == "Off"  || msgLINE == "off" ) {
-    digitalWrite(2, LOW);          // LED off
-    send_json("LED turn off");
+  else{
+    send_json("เป่าเปาไม่เข้าใจคำสั่งฮ่ะ");
   }
 }
 
@@ -79,7 +94,12 @@ void setup() {
   Serial.begin(115200);
   Serial.println("Starting...");
 
+  servo_1.attach(0); // Attaching Servo to D3
+  servo_1.write (0); // Servo will move to 45 degree angle.
+
   pinMode(2, OUTPUT);
+  pinMode(TRIGGER, OUTPUT);
+  pinMode(ECHO, INPUT);
 
   if (WiFi.begin(ssid, password)) {
     while (WiFi.status() != WL_CONNECTED) {
@@ -98,10 +118,29 @@ void setup() {
 }
 
 void loop() {
+  long duration, distance;
+  digitalWrite(TRIGGER, LOW);  
+  delayMicroseconds(2); 
+  
+  digitalWrite(TRIGGER, HIGH);
+  delayMicroseconds(10); 
+  
+  digitalWrite(TRIGGER, LOW);
+  duration = pulseIn(ECHO, HIGH);
+  distance = (duration/2) / 29.1;
+  
+  Serial.print(distance);
+  Serial.println("Centimeter:");
+  
   if (microgear.connected()) {
   //  Serial.println("NETPIE is connecting!");
     microgear.loop();
     timer = 0;
+    if(distance > 0 && distance < 10){
+       send_json("เป่าเปาหิวข้าวแล้วฮ่ะ");
+       send_json("ให้อาหารหน่อย");
+       delay(5000);
+    }
   }
   else {
     Serial.println("NETPIE is reconnecting...");
